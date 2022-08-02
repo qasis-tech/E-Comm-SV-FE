8088307e398b2b1b1868380fedfd032d826e45e6
Sabeer ali <sabeer@sectorqube.com>
2022-08-02 12:32:46 +0530

    login page -  user name input field  added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./pages/loginPage";
 
 function App() {
   return (
-    <div >
+    <div>
       {/* <Home /> */}
       {/* <ProductDetails /> */}
       <Login />
--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,30 +1,19 @@
-import * as React from 'react';
-import Checkbox from '@mui/material/Checkbox';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+import * as React from "react";
+import TextField from "@mui/material/TextField";
 
 function LoginPage() {
-    return (
-      <div >
-        <h1>LoginPage</h1>
-        <Checkbox {...label} disabled checked />
-        <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
-    </Box>
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      <TextField
+        id="login-username"
+        label="username"
+        variant="outlined"
+        size="small"
+        fullWidth
+      />
+    </div>
+  );
+}
 
-      </div>
-    );
-  }
-  
-  export default LoginPage;
+export default LoginPage;
