e99d585c5d0464394fbbe83318ce2ecb992a5c49
Geethu KN <geethu90@gmail.com>
2022-08-03 17:28:39 +0530

    register page firstname lastname email mobilenumber validation useform yup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import Home from "./pages/homePage";
 // import ProductDetails from "./pages/productDetais";
-import Login from "./pages/loginPage";
+// import Login from "./pages/loginPage";
+import RegisterPage from "./pages/registerPage";
 
 function App() {
   return (
     <div>
       {/* <Home /> */}
       {/* <ProductDetails /> */}
-      <Login />
+      {/* <Login /> */}
+      <RegisterPage />
     </div>
   );
 }
---
+++ src/pages/registerPage.js
@@ -0,0 +1,115 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Button from "@mui/material/Button";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    email: yup.string().email().required(),
+    mobilenumber: yup.number().min(10).required(),
+  })
+  .required();
+
+function RegisterPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleRegister = (data) => {
+    console.log("Data Result", data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleRegister)}>
+      <h1>REGISTRATION FORM</h1>
+      <TextField
+        id="filled-basic"
+        label="FirstName"
+        {...register("firstName")}
+        variant="filled"
+        fullWidth
+      />
+      <p>{errors?.firstName?.message}</p>
+      <TextField
+        id="filled-basic"
+        label="LastName"
+        {...register("lastName")}
+        variant="filled"
+        fullWidth
+      />
+      <p>{errors?.lastName?.message}</p>
+      <TextField
+        id="filled-basic"
+        label="email"
+        {...register("email")}
+        variant="filled"
+        fullWidth
+      />
+      <p>{errors?.email?.message}</p>
+      <TextField type="number" {...register("mobilenumber")} />
+      <p>{errors?.mobilenumber?.message}</p>
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel
+            {...register("gender", { required: true })}
+            type="radio"
+            value="female"
+            control={<Radio />}
+            label="Female"
+          />
+
+          <FormControlLabel
+            {...register("gender", { required: true })}
+            type="radio"
+            value="male"
+            control={<Radio />}
+            label="Male"
+          />
+          <FormControlLabel
+            {...register("gender", { required: true })}
+            type="radio"
+            value="other"
+            control={<Radio />}
+            label="Other"
+          />
+        </RadioGroup>
+      </FormControl>
+
+      <TextField
+        id="filled-basic"
+        label="OTP for email"
+        variant="filled"
+        fullWidth
+      />
+      <TextField
+        id="filled-basic"
+        label="OTP for mobile"
+        variant="filled"
+        fullWidth
+      />
+      <Button type="submit" variant="contained">
+        Register
+      </Button>
+    </form>
+  );
+}
+
+export default RegisterPage;
