2abc99f0562eeff37959e97877ff064a23d329d0
Vaishna K p <vaishnakp1999@gmail.com>
2022-08-05 17:50:15 +0530

    add filter,login,register page

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -16,6 +16,7 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { useForm } from "react-hook-form";
 import GoogleIcon from "@mui/icons-material/Google";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import login from "../assets/login.jpg";
 import "../styles/login.styles.scss";
 
@@ -50,118 +51,133 @@ function LoginPage() {
   };
 
   return (
-    <React.Fragment>
-      <Container className="container">
-        <Box
-          sx={{ flexGrow: 1 }}
-          noValidate
-          autoComplete="off"
-          className="wrapper"
-        >
-          <Grid container direction="row" className="login-container">
-            <Grid item xs={4} className="image-wrapper">
-              <img src={login} alt="login image" />
-            </Grid>
-            <Grid item xs={8} className="form-section">
-              <form onSubmit={handleSubmit(handleLogin)}>
-                <Grid item xs={12} className="profile-container">
-                  <h3 fw-bold>LOGIN</h3>
-                </Grid>
-                <TextField
-                  id="login-username"
-                  variant="outlined"
-                  size="small"
-                  label="Email ID"
-                  {...register("email", {
-                    required: "Email ID is required",
-                    pattern: {
-                      value:
-                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                      message: "Invalid email Id (example@mail.com) ",
-                    },
-                  })}
-                  fullWidth
-                  error={errors?.email}
-                />
-                <p>{errors?.email?.message}</p>
-
-                <TextField
-                  label="Password"
-                  size="small"
-                  fullWidth
-                  type={isVisible ? "text" : "password"}
-                  {...register("password", {
-                    required: "Password is required",
-                    minLength: {
-                      value: 8,
-                      message: "Minimum of 8 Charecter",
-                    },
-                  })}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={() => setVisible(!isVisible)}
-                          edge="end"
-                        >
-                          {!isVisible ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                  error={errors?.password}
-                />
-                <p>{errors?.password?.message}</p>
-
-                <Grid
-                  container
-                  direction="row"
-                  justifyContent="space-between"
-                  marginBottom={2}
-                >
-                  <Grid item>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          checked={isChecked}
-                          onChange={() => setCheckBox(!isChecked)}
-                        />
-                      }
-                      label="Keep me signed in"
-                    />
-                  </Grid>
-                  <Grid item sx={{ display: "flex", alignItems: "center" }}>
-                    <Typography>Forgot password?</Typography>
-                  </Grid>
-                </Grid>
-                <Grid
-                  container
-                  direction="row"
-                  justifyContent="space-between"
-                  marginBottom={2}
-                >
-                  <Grid item>
-                    <Button className="login-with-button" variant="outlined">
-                      <GoogleIcon />
-                      Login with Google
-                    </Button>
-                  </Grid>
-                  <Grid item sx={{ display: "flex", alignItems: "center" }}>
-                    <Button variant="contained">
-                      <FacebookIcon />
-                      Login with Facebook
-                    </Button>
-                  </Grid>
-                </Grid>
-                <Button type="submit " variant="contained" fullWidth>
-                  Login
-                </Button>
-              </form>
-            </Grid>
+    <Container className="container">
+      <Box
+        sx={{ flexGrow: 1 }}
+        noValidate
+        autoComplete="off"
+        className="wrapper"
+      >
+        <Grid container direction="row" className="login-container">
+          <Grid item xs={4} className="image-wrapper">
+            <img src={login} alt="login image" />
           </Grid>
-        </Box>
-      </Container>
-    </React.Fragment>
+          <Grid item xs={8} className="form-section">
+            <form onSubmit={handleSubmit(handleLogin)}>
+              <Grid item xs={12} marginBottom={3} className="profile-container">
+                <h2 fw-bold>LOGIN</h2>
+              </Grid>
+              <TextField
+                id="login-username"
+                variant="outlined"
+                size="small"
+                label="Email"
+                {...register("email", {
+                  required: "Email ID is required",
+                  pattern: {
+                    value:
+                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: "Invalid email Id (example@mail.com) ",
+                  },
+                })}
+                fullWidth
+                error={errors?.email}
+              />
+              <p>{errors?.email?.message}</p>
+
+              <TextField
+                label="Password"
+                size="small"
+                fullWidth
+                type={isVisible ? "text" : "password"}
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Minimum of 8 Charecter",
+                  },
+                })}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => setVisible(!isVisible)}
+                        edge="end"
+                      >
+                        {!isVisible ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                error={errors?.password}
+              />
+              <p>{errors?.password?.message}</p>
+
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                marginBottom={2}
+              >
+                <Grid item>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={isChecked}
+                        onChange={() => setCheckBox(!isChecked)}
+                      />
+                    }
+                    label="Keep me signed in"
+                  />
+                </Grid>
+                <Grid item sx={{ display: "flex", alignItems: "center" }}>
+                  <Typography>Forgot password?</Typography>
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                className="sub-btn"
+                justifyContent="space-between"
+                marginBottom={2}
+              >
+                <Grid item>
+                  <Button className="login-with-button" variant="outlined">
+                    <GoogleIcon />
+                    Login with Google
+                  </Button>
+                </Grid>
+                <Grid item sx={{ display: "flex", alignItems: "center" }}>
+                  <Button className="login-with-button" variant="contained">
+                    <FacebookIcon />
+                    Login with Facebook
+                  </Button>
+                </Grid>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                className="sub-btn"
+                justifyContent="space-between"
+                marginTop={4}
+              >
+                <Grid item>
+                  <Button type="submit" variant="contained">
+                    Login
+                  </Button>
+                </Grid>
+                <Grid item className="create-account">
+                  <a href="">
+                    Create your account
+                    <ArrowForwardIcon />
+                  </a>
+                </Grid>
+              </Grid>
+            </form>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 export default LoginPage;
