4d420a4c6441285a4073f32d03d56a5578e81e12
Geethu KN <geethu90@gmail.com>
2022-08-14 11:08:27 +0530

    category addpage validation and add remove subcategory

--- src/pages/admin/dashboard/categoryPages/addCategoryPage.js
+++ src/pages/admin/dashboard/categoryPages/addCategoryPage.js
@@ -1,53 +1,21 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Box, Grid } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import Fab from "@mui/material/Fab";
-<<<<<<< HEAD
 import axios from "axios";
-import { useState } from "react";
 import "./addCategoryPage.styles.scss";
 import { useForm, useFieldArray, Controller, useWatch } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
-=======
-
 import "./addCategoryPage.styles.scss";
-
-const categoryaddpageSchema = yup
-  .object()
-  .shape({
-    mainCategory: yup.string().required("Name is required"),
-    // categoryFile: yup.mixed().required("Upload a file"),
-    // .test("fileSize", "The file is too large", (value) => {
-    //   return value && value[0].size <= 2000000;
-    // })
-    // .test("type", "We only support jpeg", (value) => {
-    //   return value && value[0].type === "image/jpeg";
-    // })
-    subCategory: yup.string().required("Name is required"),
-    // file2: yup.object().required("Upload a file"),
-    // .test("fileSize", "The file is too large", (value) => {
-    //   return value && value[0].size <= 2000000;
-    // })
-    // .test("type", "We only support jpeg", (value) => {
-    //   return value && value[0].type === "image/jpeg";
-    // })
-  })
-  .required();
->>>>>>> 1c210fef94f3c3e0460042897a4251bac465bee3
 
 const AddCategory = () => {
   const {
     register,
     handleSubmit,
-<<<<<<< HEAD
     formState: { errors },
     control,
   } = useForm();
@@ -55,101 +23,8 @@ const AddCategory = () => {
     control,
     name: "subcategory",
   });
-  // const [duplicate, setduplicate] = useState([{ title: "" }]);
-  // const [subC, setSubc] = useState(0);
-  // const handleAddbutton = (event) => {
-  //   let data = [...duplicate];
-  //   data.push({ title: "" });
-  //   setduplicate(data);
-  //   setSubc((subC) => subC + 1);
-  // };
-  // const handleRemovebutton = (index) => {
-  //   const newdata = [...duplicate];
-  //   newdata.splice(index, 1);
-  //   setduplicate(newdata);
-  // };
-  // const handleCategoryAdd = ({
-  //   mainCategory,
-  //   subCategory,
-  //   categoryImage,
-  //   file2,
-  // }) => {
-  //   var bodyFormData = new FormData();
-  //   bodyFormData.append("name", mainCategory);
-  //   bodyFormData.append("image", categoryImage);
-  //   bodyFormData.append("sub1", file2);
-  //   // console.log("CategoryAddpage Details:", data);
-  //   console.log("category", mainCategory);
-  //   console.log("subcategory=>=>", subCategory);
-  //   axios
-  //     .post("http://localhost:4000/category", bodyFormData, {
-  //       headers: { "Content-Type": "multipart/form-data" },
-  //     })
-  //     .then((response) => {
-  //       console.log("Response=>>", response);
-  //     })
-  //     .catch((err) => {
-  //       //handle error
-  //       console.log("Errorss=>>", err);
-  //     });
-  // };
-=======
-    watch,
-    getValues,
-    setValue,
-    getFieldState,
-    formState: { errors, isDirty },
-  } = useForm({
-    resolver: yupResolver(categoryaddpageSchema),
-    mode: "onChange",
-    defaultValues: {
-      categoryFile: null,
-    },
-  });
   const navigate = useNavigate();
-  const [duplicate, setduplicate] = useState([{ title: "" }]);
-  const [subC, setSubc] = useState(0);
-  const handleAddbutton = (event) => {
-    let data = [...duplicate];
-    data.push({ title: "" });
-    setduplicate(data);
-    setSubc((subC) => subC + 1);
-  };
-  const handleRemovebutton = (index) => {
-    const newdata = [...duplicate];
-    newdata.splice(index, 1);
-    setduplicate(newdata);
-  };
 
-  const handleSubmitApi = (bodyFormData) => {
-    axios
-      .post("http://localhost:4000/category", bodyFormData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      })
-      .then((response) => {
-        console.log("Response=>>", response);
-        navigate(-1);
-      })
-      .catch((err) => {
-        //handle error
-        console.log("Errorss in add category -> submit api", err);
-      });
-  };
-
-  const handleCategoryAdd = ({ mainCategory, subCategory, file1, file2 }) => {
-    var bodyFormData = new FormData();
-    bodyFormData.append("name", mainCategory);
-    bodyFormData.append("image", file1);
-    bodyFormData.append("sub1", file2);
-    // console.log("CategoryAddpage Details:", data);
-    console.log("category", mainCategory);
-    console.log("subcategory=>=>", subCategory);
-    // handleSubmitApi(bodyFormData);
-  };
-
-  console.log("Watch", watch("file2"));
-
->>>>>>> 1c210fef94f3c3e0460042897a4251bac465bee3
   return (
     <div className="add-category">
       <Box noValidate autoComplete="off" className="wrapper">
@@ -170,7 +45,6 @@ const AddCategory = () => {
                   name="mainCategory"
                   render={({ message }) => <p>{message}</p>}
                 />
-<<<<<<< HEAD
                 <Button variant="contained" fullWidth component="label">
                   Upload Image
                   <input
@@ -187,41 +61,6 @@ const AddCategory = () => {
                   name="imageCategory"
                   render={({ message }) => <p>{message}</p>}
                 />
-
-=======
-                <p>{errors?.mainCategory?.message}</p>
-                {getValues("categoryFile[0].name") && (
-                  <div className="d-flex justify-content-between my-3 border-bottom pb-2">
-                    <div>{getValues("categoryFile[0].name")}</div>
-                    <div>
-                      <button
-                        className="btn btn-primary"
-                        onClick={() => setValue("categoryFile", null)}
-                      >
-                        x
-                      </button>
-                    </div>
-                  </div>
-                )}
-                <Button variant="contained" fullWidth component="label">
-                  Upload Image
-                  <input
-                    type="file"
-                    hidden
-                    {...register("categoryFile", { required: true })}
-                  />
-                </Button>
-
-                {/* <p>{errors?.file1?.message}</p> */}
->>>>>>> 1c210fef94f3c3e0460042897a4251bac465bee3
-                {/* <TextField
-                  fullWidth
-                  id="outlined-required"
-                  label="Upload Image"
-                  type="file"
-                  {...register("file1")}
-                />
-                <p>{errors?.file1?.message}</p> */}
               </div>
               <Grid container spacing={2} marginTop={4}>
                 <Grid container spacing={2} paddingLeft={2}>
@@ -239,19 +78,10 @@ const AddCategory = () => {
                 {fields.map((list, index) => {
                   return (
                     <Grid key={list.id} item xs={12}>
-                      {/* {duplicate.length - 1 === index && (
-                            <AddIcon
-                              color="primary"
-                              style={{ fontSize: 25, backgroundColor: "red" }}
-                              onClick={(e) => handleAddbutton(e)}
-                            />
-                          )} */}
-
                       <TextField
                         fullWidth
                         variant="outlined"
                         label="Name"
-<<<<<<< HEAD
                         {...register(`subCategoryName ${index}`)}
                       />
 
@@ -266,18 +96,6 @@ const AddCategory = () => {
                       </Button>
 
                       {fields.length > 1 && (
-=======
-                        // {...register("subCategory")}
-                        // error={errors?.subCategory}
-                      />
-                      {/* <p>{errors?.subCategory?.message}</p> */}
-                      <Button variant="contained" fullWidth component="label">
-                        Upload Image
-                        <input type="file" hidden />
-                      </Button>
-                      {/* <p>{errors?.file2?.message}</p> */}
-                      {duplicate.length > 1 && (
->>>>>>> 1c210fef94f3c3e0460042897a4251bac465bee3
                         <button
                           onClick={() => remove(index)}
                           className="btn btn-primary"
