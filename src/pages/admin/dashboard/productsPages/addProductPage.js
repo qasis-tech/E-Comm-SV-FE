e97c13b4bea65ccfb017648a253e6a83f1228354
Geethu KN <geethu90@gmail.com>
2022-08-18 15:37:54 +0530

    addproduct api feature add

--- src/pages/admin/dashboard/productsPages/addProductPage.js
+++ src/pages/admin/dashboard/productsPages/addProductPage.js
@@ -1,11 +1,13 @@
 import * as React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray, Controller, useWatch } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { URLS } from "../../../../config/urls.config";
+import { ErrorMessage } from "@hookform/error-message";
+
 import "./add-product.styles.scss";
 
 import {
@@ -30,6 +32,8 @@ const productaddSchema = yup
       .typeError("You must specify number")
       .min(0, "Min value 0"),
     description: yup.string().required("Description is required"),
+    featureKey: yup.string(),
+    featureValue: yup.string(),
     price: yup.number().typeError("You must specify number").required(),
     offerQuantity: yup
       .number()
@@ -41,8 +45,8 @@ const productaddSchema = yup
       .required("OfferPrice is required")
       .typeError("You must specify number")
       .min(0, "Min value 0"),
-    imageFile: yup.mixed().required("Upload a file"),
-    videoFile: yup.mixed().required("Upload a file"),
+    productImageFile: yup.string().required(),
+    productVideoFile: yup.string().required(),
   })
   .required();
 
@@ -51,9 +55,24 @@ const AddProduct = () => {
     register,
     handleSubmit,
     watch,
+    control,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(productaddSchema),
+    defaultValues: {
+      features: [{ featureKey: "", featureValue: "" }],
+    },
+  });
+  const { fields, append } = useFieldArray({
+    control,
+    name: "features",
+  });
+  const watchFieldArray = watch("features");
+  const controlledFields = fields?.map((field, index) => {
+    return {
+      ...field,
+      ...watchFieldArray[index],
+    };
   });
 
   const [categoryData, setCategorydata] = useState([]);
@@ -94,6 +113,7 @@ const AddProduct = () => {
   const handleOfferUnit = (e, val) => setSelectedofferunit(val);
 
   const handleFeatureKey = (key, val) => {
+    console.log("key", key);
     let temp = [...featureData];
     let obj = {};
     obj[key.target.value] = val?.target?.value;
@@ -101,35 +121,53 @@ const AddProduct = () => {
     SetFeaturedata(temp);
   };
 
-  const handleProductAdd = ({
-    productName,
-    quantity,
-    selectedUnit,
-    selectedCategory,
-    selectedSubCategory,
-    description,
-    features,
-    price,
-    selectedOfferunit,
-    offerQuantity,
-    offerPrice,
-    imageFile,
-    videoFile,
-  }) => {
+  const handleProductAdd = (data) => {
+    const {
+      productName,
+      quantity,
+      selectedUnit,
+      selectedCategory,
+      selectedSubCategory,
+      description,
+      features,
+      price,
+      selectedOfferunit,
+      offerQuantity,
+      offerPrice,
+      productImageFile,
+      productVideoFile,
+    } = data;
     var bodyFormData = new FormData();
     bodyFormData.append("name", productName);
-    bodyFormData.append("category", selectedCategory.label);
-    bodyFormData.append("subCategory", selectedSubCategory.label);
-    bodyFormData.append("unit", selectedUnit.label);
+    bodyFormData.append("category", selectedCategory);
+    bodyFormData.append("subCategory", selectedSubCategory);
+    bodyFormData.append("unit", selectedUnit);
     bodyFormData.append("quantity", quantity);
     bodyFormData.append("description", description);
     bodyFormData.append("price", price);
-    bodyFormData.append("offerUnit", selectedOfferunit.label);
+    bodyFormData.append("offerUnit", selectedOfferunit);
     bodyFormData.append("offerQuantity", offerQuantity);
     bodyFormData.append("offerPrice", offerPrice);
-    bodyFormData.append("productImage", imageFile);
-    bodyFormData.append("productVideo", videoFile);
 
+    console.log("productimage", productImageFile);
+    for (let i = 0; i < productImageFile.length; i++) {
+      // bodyFormData.append("productImage", productImageFile[i]);
+      console.log("productfile", productImageFile[i]);
+    }
+
+    // bodyFormData.append("productVideo", productVideoFile);
+
+    const temp = {};
+    const featureArray = [];
+
+    for (const values of features) {
+      let key = values.featureKey;
+      temp[key] = values.featureValue;
+      // bodyFormData.append(`${values.featureKey}`, values.featureValue);
+    }
+    featureArray.push(temp);
+    bodyFormData.append("features", featureArray);
+    console.log("features=>>>", featureArray);
     const token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdlZXRodUB0ZXN0LmNvbSIsImlhdCI6MTY2MDI5NzkwNiwiZXhwIjoxNjYxMTYxOTA2fQ.qhDBNneysBl7A_MRi-0f0t8nsq034wp07EODXDEh2Eg";
 
@@ -148,7 +186,6 @@ const AddProduct = () => {
       });
   };
 
-  console.log("featureData", featureData);
   const navigate = useNavigate();
 
   return (
@@ -161,7 +198,7 @@ const AddProduct = () => {
           className="add-product-container"
         >
           <div className="product-form-section col-md-8">
-            <form onSubmit={handleSubmit(handleProductAdd)}>
+            <form onSubmit={handleSubmit((res) => handleProductAdd(res))}>
               <div className="main-product-heading">
                 <h3 className="product-heading">Product</h3>
               </div>
@@ -204,17 +241,17 @@ const AddProduct = () => {
                 </Grid>
                 <Grid container spacing={2}>
                   <Grid item xs={6}>
-                    {unitData?.length && (
+                    {categoryData?.length && (
                       <Autocomplete
-                        options={unitData}
+                        options={categoryData}
                         getOptionLabel={(option) => option.label || ""}
                         isOptionEqualToValue={(option, value) =>
-                          option.label === value.label
+                          option._id === value._id
                         }
-                        onChange={(e, val) => handleUnit(e, val)}
-                        value={selectedUnit}
+                        onChange={(e, val) => handleCategory(e, val)}
+                        value={selectedCategory}
                         renderInput={(params) => (
-                          <TextField {...params} label="Category" />
+                          <TextField {...params} label="Categories" />
                         )}
                       />
                     )}
@@ -251,27 +288,40 @@ const AddProduct = () => {
                     <p>{errors?.description?.message}</p>
                   </Grid>
                 </Grid>
-                <Grid container spacing={2}>
-                  <Grid item xs={6}>
-                    <TextField
-                      fullWidth
-                      id="outlined-multiline-static"
-                      label="Features"
-                      onChange={(e) => handleFeatureKey(e, null)}
-                    />
-                  </Grid>
-                  <Grid item xs={5}>
-                    <TextField
-                      fullWidth
-                      id="outlined-multiline-static"
-                      label="value"
-                      onChange={(e) => handleFeatureKey(null, e)}
-                    />
-                  </Grid>
-                  <Grid item xs={1}>
-                    <AddIcon color="primary" className="add-icon-section" />
-                  </Grid>
-                </Grid>
+                {controlledFields?.map((list, index) => {
+                  return (
+                    <Grid key={list.id} container spacing={2}>
+                      <Grid item xs={6}>
+                        <TextField
+                          fullWidth
+                          id="outlined-multiline-static"
+                          label="Features"
+                          onChange={(e) => handleFeatureKey(e, null)}
+                          {...register(`features.${index}.featureKey`)}
+                        />
+                      </Grid>
+                      <Grid item xs={5}>
+                        <TextField
+                          fullWidth
+                          id="outlined-multiline-static"
+                          label="value"
+                          onChange={(e) => handleFeatureKey(null, e)}
+                          {...register(`features.${index}.featureValue`)}
+                        />
+                      </Grid>
+
+                      <Grid item xs={1}>
+                        <AddIcon
+                          onClick={() =>
+                            append({ featureKey: "", featureValue: "" })
+                          }
+                          color="primary"
+                          className="add-icon-section"
+                        />
+                      </Grid>
+                    </Grid>
+                  );
+                })}
                 <Grid container marginTop={1} spacing={2}>
                   <Grid item xs={6}>
                     <TextField
@@ -324,25 +374,33 @@ const AddProduct = () => {
                     <Button variant="contained" fullWidth component="label">
                       Upload Image
                       <input
-                        {...register("imageCategory", {
-                          required: "This is required.",
-                        })}
+                        {...register("productImageFile")}
                         type="file"
                         hidden
                       />
                     </Button>
+                    <ErrorMessage
+                      errors={errors}
+                      name="productImageFile"
+                      render={({ message }) => <p>{message}</p>}
+                    />
                   </Grid>
                   <Grid item xs={6}>
                     <Button variant="contained" fullWidth component="label">
                       Upload Video
                       <input
-                        {...register("imageCategory", {
+                        {...register("productVideoFile", {
                           required: "This is required.",
                         })}
                         type="file"
                         hidden
                       />
                     </Button>
+                    <ErrorMessage
+                      errors={errors}
+                      name="productVideoFile"
+                      render={({ message }) => <p>{message}</p>}
+                    />
                   </Grid>
                 </Grid>
               </div>
@@ -367,192 +425,6 @@ const AddProduct = () => {
         </Grid>
       </Box>
     </div>
-    // <React.Fragment>
-    //   <Container maxWidth="md">
-    //     <Box sx={{}} autoComplete="off">
-    //       <form onSubmit={handleSubmit(handleProductAdd)}>
-    //         <Grid container spacing={2}>
-    //           <Grid item xs={4}>
-    //             <TextField
-    //               fullWidth
-    //               variant="outlined"
-    //               label="Name"
-    //               {...register("productName")}
-    //             />
-    //             <p>{errors?.productName?.message}</p>
-    //           </Grid>
-    //           <Grid item xs={4}>
-    //             <TextField
-    //               fullWidth
-    //               variant="outlined"
-    //               label="Quantity"
-    //               {...register("quantity")}
-    //             />
-    //             <p>{errors?.quantity?.message}</p>
-    //           </Grid>
-    //           <Grid item xs={4}>
-    //             {unitData?.length && (
-    //               <Autocomplete
-    //                 options={unitData}
-    //                 getOptionLabel={(option) => option.label || ""}
-    //                 isOptionEqualToValue={(option, value) =>
-    //                   option.label === value.label
-    //                 }
-    //                 onChange={(e, val) => handleUnit(e, val)}
-    //                 value={selectedUnit}
-    //                 renderInput={(params) => (
-    //                   <TextField {...params} label="Units" />
-    //                 )}
-    //               />
-    //             )}
-    //           </Grid>
-    //         </Grid>
-    //         <Grid container spacing={2}>
-    //           <Grid item xs={6}>
-    //             {categoryData?.length && (
-    //               <Autocomplete
-    //                 options={categoryData}
-    //                 getOptionLabel={(option) => option.label || ""}
-    //                 isOptionEqualToValue={(option, value) =>
-    //                   option._id === value._id
-    //                 }
-    //                 onChange={(e, val) => handleCategory(e, val)}
-    //                 value={selectedCategory}
-    //                 renderInput={(params) => (
-    //                   <TextField {...params} label="Categories" />
-    //                 )}
-    //               />
-    //             )}
-    //           </Grid>
-    //           <Grid item xs={6}>
-    //             <Autocomplete
-    //               options={
-    //                 selectedCategory?.subCategory?.length
-    //                   ? selectedCategory?.subCategory
-    //                   : []
-    //               }
-    //               getOptionLabel={(option) => option.label || ""}
-    //               isOptionEqualToValue={(option, value) =>
-    //                 option.label === value.label
-    //               }
-    //               onChange={(e, val) => handleSubCategory(e, val)}
-    //               value={selectedSubCategory}
-    //               renderInput={(params) => (
-    //                 <TextField {...params} label="Subcategories" />
-    //               )}
-    //             />
-    //           </Grid>
-    //         </Grid>
-    //         <Grid>
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               fullWidth
-    //               id="outlined-multiline-static"
-    //               label="Description"
-    //               multiline
-    //               rows={4}
-    //               {...register("description")}
-    //             />
-    //             <p>{errors?.description?.message}</p>
-    //           </Grid>
-    //           {featureData.length &&
-    //             featureData.map((item, index) => {
-    //               return (
-    //                 <Grid key={index} container spacing={2}>
-    //                   <Grid item xs={4}>
-    //                     <TextField
-    //                       fullWidth
-    //                       id="outlined-multiline-static"
-    //                       label="name"
-    //                       onChange={(e) => handleFeatureKey(e, null)}
-    //                     />
-    //                   </Grid>
-    //                   <Grid item xs={4}>
-    //                     <TextField
-    //                       fullWidth
-    //                       id="outlined-multiline-static"
-    //                       label="value"
-    //                       onChange={(e) => handleFeatureKey(null, e)}
-    //                       value={item}
-    //                     />
-    //                   </Grid>
-    //                   <Grid item xs={4}>
-    //                     <Button variant="contained">+</Button>
-    //                   </Grid>
-    //                 </Grid>
-    //               );
-    //             })}
-    //           <Divider />
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               fullWidth
-    //               variant="outlined"
-    //               label="Price"
-    //               {...register("price")}
-    //             />
-    //             <p>{errors?.price?.message}</p>
-    //           </Grid>
-    //           <Grid item xs={4}>
-    //             {unitData?.length && (
-    //               <Autocomplete
-    //                 options={unitData}
-    //                 getOptionLabel={(option) => option.label || ""}
-    //                 isOptionEqualToValue={(option, value) =>
-    //                   option.label === value.label
-    //                 }
-    //                 onChange={(e, val) => handleOfferUnit(e, val)}
-    //                 value={selectedOfferunit}
-    //                 renderInput={(params) => (
-    //                   <TextField {...params} label="Units" />
-    //                 )}
-    //               />
-    //             )}
-    //           </Grid>
-    //           <Grid item xs={4}>
-    //             <TextField
-    //               fullWidth
-    //               variant="outlined"
-    //               label="Quantity"
-    //               {...register("offerQuantity")}
-    //             />
-    //             <p>{errors?.offerQuantity?.message}</p>
-    //           </Grid>
-    //           <Grid item xs={4}>
-    //             <TextField
-    //               fullWidth
-    //               variant="outlined"
-    //               label="Offer Price"
-    //               {...register("offerPrice")}
-    //             />
-    //             <p>{errors?.offerPrice?.message}</p>
-    //           </Grid>
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               fullWidth
-    //               variant="outlined"
-    //               type="file"
-    //               {...register("imageFile")}
-    //             />
-    //             {errors?.imageFile && <p>{errors?.imageFile?.message}</p>}
-    //           </Grid>
-    //           <Grid item xs={12}>
-    //             <TextField
-    //               fullWidth
-    //               variant="outlined"
-    //               type="file"
-    //               {...register("videoFile")}
-    //             />
-    //             {errors?.videoFile && <p>{errors?.videoFile?.message}</p>}
-    //           </Grid>
-    //         </Grid>
-    //         <Button variant="outlined">Cancel</Button>
-    //         <Button type="submit" variant="contained" color="primary">
-    //           submit
-    //         </Button>
-    //       </form>
-    //     </Box>
-    //   </Container>
-    // </React.Fragment>
   );
 };
 
